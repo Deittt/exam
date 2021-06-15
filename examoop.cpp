637d7b2fa337d54c1a3d0becc61c1d91913bb175
Deittt <lavless63@gmail.com>
2021-06-15 09:21:24 +0300

    find t

--- examoop.cpp
+++ examoop.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 
+void find_t(string t) {
+    char l = 't';
+    int sum = 0;
+    for (int i = 0; i < t.size(); i++)
+    {
+        if (t[i] == l) {
+            sum += 1;
+        }
+
+    }
+    cout << sum << endl;
+}
+
 int main()
 {
-    cout << "Hello, world!" << endl;
+    string str;
+    cin >> str;
+    find_t(str);
    
     return 0;
 }
