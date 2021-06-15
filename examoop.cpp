073fc14aeefaa85e75b919794fdab72d7c6a5d7b
Deittt <lavless63@gmail.com>
2021-06-15 09:24:49 +0300

    find te

--- examoop.cpp
+++ examoop.cpp
@@ -16,11 +16,27 @@ void find_t(string t) {
     cout << sum << endl;
 }
 
+void find_te(string t) {
+    char l = 't';
+    char k = 'e';
+    int sum = 0;
+    for (int i = 0; i < t.size(); i++)
+    {
+        if (t[i] == l) {
+            if(t[i+1]!=k)
+            sum += 1;
+        }
+
+    }
+    cout << sum << endl;
+}
+
 int main()
 {
     string str;
     cin >> str;
     find_t(str);
+    find_te(str);
    
     return 0;
 }
